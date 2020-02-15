9a084876b86b4071e02ca34fe3f6b6837095d6af
Lunar Capital <lunarcapital@Dylans-MacBook-Air-1006.local>
2020-02-15 22:37:52 +0800

    finished tile controller. ledge superimposition done. now cleaning up world controller into smaller modules.

---
+++ scripts/Controllers/WorldControllerUtilities/PositionTracker.gd
@@ -0,0 +1,22 @@
+extends Node2D
+"""
+Tracks and records world-related data of all entities such as:
+	Current Z-index (tilemap)
+	Current Tile (and adjacent tiles)
+	
+"""
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
